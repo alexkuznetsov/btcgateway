448f5af186700892110491f7e87e75c91df1818b
Alexandr Kuznetsov <kuznetsovshurik@gmail.com>
2019-04-20 22:10:35 +0600

    improve readability;

--- BTCGatewayAPI.Bitcoin/RPCServer.cs
+++ BTCGatewayAPI.Bitcoin/RPCServer.cs
@@ -27,37 +27,44 @@ namespace BTCGatewayAPI.Bitcoin
 
         public Task<List<Unspent>> ListUnspentAsync(string address)
         {
-            return ExecuteRequestAsync<ListUnspent, ListUnspentResponse, List<Unspent>>(new ListUnspent(addresses: new string[] { address }));
+            return ExecuteRequestAsync<ListUnspent, ListUnspentResponse, List<Unspent>>(
+                new ListUnspent(addresses: new string[] { address }));
         }
 
         public Task<string> RemovePrunedFundsAsync(string txHash)
         {
-            return ExecuteRequestAsync<RemovePrunedFundsRequest, RemovePrunedFundsResponse, string>(new RemovePrunedFundsRequest(txHash));
+            return ExecuteRequestAsync<RemovePrunedFundsRequest, RemovePrunedFundsResponse, string>(
+                new RemovePrunedFundsRequest(txHash));
         }
 
         public Task<string> SendRawTransactionAsync(string txHash)
         {
-            return ExecuteRequestAsync<SendRawTransactionRequest, SendRawTransactionResponse, string>(new SendRawTransactionRequest(txHash));
+            return ExecuteRequestAsync<SendRawTransactionRequest, SendRawTransactionResponse, string>(
+                new SendRawTransactionRequest(txHash));
         }
 
         public Task<EstimateSmartFee> EstimateSmartFeeAsync(int confTarget)
         {
-            return ExecuteRequestAsync<GetEstimateSmartFeeRequest, GetEstimateSmartFeeResponce, EstimateSmartFee>(new GetEstimateSmartFeeRequest(confTarget));
+            return ExecuteRequestAsync<GetEstimateSmartFeeRequest, GetEstimateSmartFeeResponce, EstimateSmartFee>(
+                new GetEstimateSmartFeeRequest(confTarget));
         }
 
         public Task<string> CreateRawtransactionAsync(TXInfo[] inputs, Dictionary<string, decimal> outputs)
         {
-            return ExecuteRequestAsync<CreateRawTransaction, CreateRawTransactionResponse, string>(new CreateRawTransaction(inputs/*AsStr*/, outputs/*AsStr*/));
+            return ExecuteRequestAsync<CreateRawTransaction, CreateRawTransactionResponse, string>(
+                new CreateRawTransaction(inputs/*AsStr*/, outputs/*AsStr*/));
         }
 
         public Task<SignTransactionResult> SignRawTransactionWithKeyAsync(string transaxtionHash, string[] privateKeys, TxOutput[] outouts)
         {
-            return ExecuteRequestAsync<SignTransactionRequest, SignTransactionResponse, SignTransactionResult>(new SignTransactionRequest(transaxtionHash, privateKeys, outouts));
+            return ExecuteRequestAsync<SignTransactionRequest, SignTransactionResponse, SignTransactionResult>(
+                new SignTransactionRequest(transaxtionHash, privateKeys, outouts));
         }
 
         public Task<string> WalletPassphraseAsync(string passphrase, int seconds)
         {
-            return ExecuteRequestAsync<WalletPassphraseRequest, WalletPassphraseResponse, string>(new WalletPassphraseRequest(passphrase, seconds));
+            return ExecuteRequestAsync<WalletPassphraseRequest, WalletPassphraseResponse, string>(
+                new WalletPassphraseRequest(passphrase, seconds));
         }
 
         public Task<string> DumpPrivKeyAsync(string address)
